c2f277ae8b2f5ddb875fbcdf3396846d6b4b3660
rhe <qbbqbbb@gmail.com>
2025-05-12 21:08:32 +0200

    added more wordle logic added enter and backspace button handling added filler code to check for solution

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -7,6 +7,10 @@ const words = document.getElementsByClassName("word");
 Array.from(words[0].children).forEach((child) => {
   child.disabled = false;
 });
+let activeRow = 0;
+
+let wordlist = ["APPLE", "GRAPE", "LEMON", "MANGO", "PEACH"];
+let solution = "APPLE";
 
 let row = 1;
 Array.from(words).forEach((element) => {
@@ -16,25 +20,57 @@ Array.from(words).forEach((element) => {
     col++;
     child.addEventListener("keydown", (event) => {
       event.preventDefault();
+      let index = parseInt(child.id.slice(-1));
       if (event.key === "Backspace") {
-        let index = parseInt(child.id.slice(-1));
-        if (index > 1) {
-          let prev = document.getElementById(child.id.slice(0, -1) + `${index - 1}`);
-          if ((index === 5 && child.value == "") || index !== 5) {
-            prev.value = "";
-          }
-          prev.focus();
+        if (index === 1 || child.value !== "") {
           child.value = "";
+          child.focus();
+        } else {
+          let prev = document.getElementById(child.id.slice(0, -1) + `${index - 1}`);
+          prev.value = "";
+          prev.focus();
         }
-      }
-      if (/^[a-zA-Z]$/.test(event.key)) {
+      } else if (/^[a-zA-Z]$/.test(event.key)) {
         let letter = event.key.toUpperCase();
-        let index = parseInt(child.id.slice(-1));
         child.value = letter;
-        child.style.animation = "spin 0.6s ease normal";
+        let inputAnimation = "popIn 0.4s ease-in-out";
+        child.style.animation = inputAnimation;
         if (index < 5) {
           let next = document.getElementById(child.id.slice(0, -1) + `${index + 1}`);
           next.focus();
+        }
+      } else if (event.key === "Enter") {
+        let word = "";
+        for (let i = 1; i <= 5; i++) {
+          word += document.getElementById(child.id.slice(0, 7) + `${i}`).value;
+        }
+        if (wordlist.includes(word)) {
+          if (word === solution) {
+            console.log("Correct word!");
+            // TODO: implement win
+          } else {
+            for (let i = 1; i <= 5; i++) {
+              // TODO: check every letter and color it accordingly
+              // idea: color only the border
+              // word[i - 1] === solution[i - 1]
+            }
+            Array.from(words[activeRow].children).forEach((c) => {
+              c.disabled = true;
+            });
+            if (activeRow < 5) {
+              let nextRow = activeRow + 1;
+              Array.from(words[nextRow].children).forEach((c) => {
+                c.disabled = false;
+              });
+              document.getElementById(child.id.slice(0, -2) + `${nextRow + 1}` + "1").focus();
+              activeRow++;
+            } else {
+              console.log("Game Over");
+            }
+          }
+        } else {
+          console.log("Invalid word!");
+          // TODO: wrong word animation
         }
       }
     });
